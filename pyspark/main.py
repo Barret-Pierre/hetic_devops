d0316e73ab1988a69fbfffb5711e771df033f99d
Barret-Pierre <pierrealexandrebarret.pro@gmail.com>
2024-03-13 10:23:21 +0100

    feat(Pyspark): Add FastAPI to request python app

---
+++ pyspark/main.py
@@ -0,0 +1,15 @@
+import os
+from pyspark.sql import SparkSession
+from fastapi import FastAPI
+
+app = FastAPI()
+
+
+@app.get("/")
+def read_root():
+    # Variables d'environnement
+    spark_master = os.environ["SPARK_MASTER"]
+
+    # Connection au cluster Apache Spark
+    SparkSession.builder.master(spark_master).getOrCreate().stop()
+    return "Hello World Spark is connected"
